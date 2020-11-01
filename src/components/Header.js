937d628b359b2b7701acd9ceeb72c96bb4631a76
Eileen Li <eileenlimur@gmail.com>
2020-11-01 11:42:06 -0500

    add onClick event handler to prevent redirection

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './Header.css'
 
 export default function() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <header>
     <img alt='sneaker-head logo' src='assets/logo/sn-logo-h.png'/>
     <nav>
-      <button>Top 10</button>
-      <button>Blog</button>
-      <button>About</button>
-      <button>Connect</button>
+      <button onClick={handleSubmit}>Top 10</button>
+      <button onClick={handleSubmit}>Blog</button>
+      <button onClick={handleSubmit}>About</button>
+      <button onClick={handleSubmit}>Connect</button>
     </nav>
   </header>
   )

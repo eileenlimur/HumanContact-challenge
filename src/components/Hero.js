3cbe2fb8bf0d934fba342daa4a6d0f9a0d3179a8
Eileen Li <eileenlimur@gmail.com>
2020-11-01 11:42:47 -0500

    add useEffect to replace display image when heronav thumbnail is clicked

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeroNav from './HeroNav';
 import './Hero.css';
 
 export default function () {
+  const [heroShoe, setHeroShoe] = useState('red')
+  const [imageSource, setImageSource] = useState('/assets/backgrounds/red-shoe.png')
+
+  useEffect(()=>{
+    switch(heroShoe) {
+      case 'red':
+        setImageSource('/assets/backgrounds/red-shoe.png');
+        break;
+      case 'green':
+        setImageSource('/assets/backgrounds/green-shoe.png');
+        break;
+      case 'peach':
+        setImageSource('/assets/backgrounds/peach-shoe.png');
+        break;
+      case 'blue':
+        setImageSource('/assets/backgrounds/blue-shoe.png');
+        break;
+      }
+  },[heroShoe])
+
   return (
     <div className='hero'>
-      <img alt='red sneaker' src='/assets/backgrounds/red-shoe.png'/>
+      <img className='hero-image' alt='red sneaker' src={imageSource}/>
       <div className='hero-text'>
         <h1>We <span>Really</span> love shoes</h1>
         <h2>Sneakerhead wears, reviews and rates all the latest and greatest kicks on the market.</h2>
       </div>
-      <HeroNav />
+      <HeroNav displayShoe={heroShoe} selectShoe={setHeroShoe}/>
     </div>
   )
 }

9ebc64bd09e9e016f5121b3e917ba83eaa73057c
Eileen Li <eileenlimur@gmail.com>
2020-10-31 21:22:32 -0400

    Create HeroNav (incomplete)

---
+++ src/components/HeroNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeroNav.css';
+
+export default function HeroNav() {
+  return (
+    <nav>
+      <img alt='red shoe' src='/assets/backgrounds/red-shoe.png' />
+      <img alt='green shoe' src='/assets/backgrounds/green-shoe.png' />
+      <img alt='peach shoe' src='/assets/backgrounds/peach-shoe.png' />
+      <img alt='blue shoe' src='/assets/backgrounds/blue-shoe.png' />
+    </nav>
+  )
+}

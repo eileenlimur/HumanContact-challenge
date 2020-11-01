95b094efc9ee31db07b5e08553ebdf4970f755c2
Eileen Li <eileenlimur@gmail.com>
2020-11-01 11:41:33 -0500

    populate footer with social buttons and email update form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,30 @@ import React from 'react';
 import './Footer.css';
 
 export default function() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <footer>
-
+      <div className='social-buttons'>
+        <button className='social-button' onClick={handleSubmit}>
+          <i class="fab fa-instagram"></i>
+        </button >
+        <button className='social-button' onClick={handleSubmit}>
+          <i class="fab fa-twitter"></i>
+        </button>
+        <button className='social-button' onClick={handleSubmit}>
+          <i class="fab fa-facebook-f"></i>
+        </button>
+      </div>
+      <form>
+        <input placeholder='Enter Email. Stay up-to-date.'/>
+        <button onClick={handleSubmit}>
+          Signup
+        </button>
+      </form>
     </footer>
   )
 }

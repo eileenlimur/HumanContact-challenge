033ea20427e5f750e0be34cee0ed01d574a6eb34
Eileen Li <eileenlimur@gmail.com>
2020-10-31 21:20:40 -0400

    Refactor to move primary components to components/

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Header from './components/header/Header';
-import './App.module.css';
+import Header from './components/Header';
+import Hero from './components/Hero'
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <body className="App">
       <Header/>
-      <footer>
-
-      </footer>
+      <Hero/>
+      <Footer/>
     </body>
   );
 }

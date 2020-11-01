89947c7e9db7004a841f42cb884205a89c3f6fb6
Eileen Li <eileenlimur@gmail.com>
2020-11-01 11:43:19 -0500

    wrap images in button tags

--- src/components/HeroNav.js
+++ src/components/HeroNav.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './HeroNav.css';
 
-export default function HeroNav() {
+export default function HeroNav({ displayShoe, selectShoe }) {
+  
   return (
     <nav>
-      <img alt='red shoe' src='/assets/backgrounds/red-shoe.png' />
-      <img alt='green shoe' src='/assets/backgrounds/green-shoe.png' />
-      <img alt='peach shoe' src='/assets/backgrounds/peach-shoe.png' />
-      <img alt='blue shoe' src='/assets/backgrounds/blue-shoe.png' />
+      <button className='hero-nav-thumbnail' selected={displayShoe='red'} onClick={()=>selectShoe('red')}>
+        <img className='hero-nav-thumbnail-image' alt='red shoe thumbnail' src='/assets/backgrounds/thumbnails/red-shoe-thumbnail.png' />
+      </button>
+      <button className='hero-nav-thumbnail' selected={displayShoe='green'} onClick={()=>selectShoe('green')}>
+        <img className='hero-nav-thumbnail-image' alt='green shoe thumbnail' src='/assets/backgrounds/thumbnails/green-shoe-thumbnail.png' />
+      </button>
+      <button className='hero-nav-thumbnail' selected={displayShoe='peach'} onClick={()=>selectShoe('peach')}>
+        <img className='hero-nav-thumbnail-image higher' alt='peach shoe thumbnail' src='/assets/backgrounds/thumbnails/peach-shoe-thumbnail.png' />
+      </button>
+      <button className='hero-nav-thumbnail' selected={displayShoe='blue'} onClick={()=>selectShoe('blue')}>
+        <img className='hero-nav-thumbnail-image higher' alt='blue shoe thumbnail' src='/assets/backgrounds/thumbnails/blue-shoe-thumbnail.png' />
+      </button>
     </nav>
   )
 }

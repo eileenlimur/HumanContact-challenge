dd5e76ea271c2f886933d70dfb981d9eb24a6c27
Eileen Li <eileenlimur@gmail.com>
2020-11-01 20:14:14 -0500

    add conditional className "selected", add "peach" "blue" individual className for individual styling

--- src/components/HeroNav.js
+++ src/components/HeroNav.js
@@ -5,17 +5,17 @@ export default function HeroNav({ displayShoe, selectShoe }) {
   
   return (
     <nav>
-      <button className='hero-nav-thumbnail' selected={displayShoe='red'} onClick={()=>selectShoe('red')}>
-        <img className='hero-nav-thumbnail-image' alt='red shoe thumbnail' src='/assets/backgrounds/thumbnails/red-shoe-thumbnail.png' />
+      <button className='hero-nav-thumbnail' onClick={()=>selectShoe('red')}>
+        <img className={'hero-nav-thumbnail-image ' + (displayShoe === 'red' ? 'selected' : '')} alt='red shoe thumbnail' src='/assets/backgrounds/thumbnails/red-shoe-thumbnail.png' />
       </button>
-      <button className='hero-nav-thumbnail' selected={displayShoe='green'} onClick={()=>selectShoe('green')}>
-        <img className='hero-nav-thumbnail-image' alt='green shoe thumbnail' src='/assets/backgrounds/thumbnails/green-shoe-thumbnail.png' />
+      <button className='hero-nav-thumbnail' onClick={()=>selectShoe('green')}>
+        <img className={'hero-nav-thumbnail-image ' + (displayShoe === 'green' ? 'selected' : '')} alt='green shoe thumbnail' src='/assets/backgrounds/thumbnails/green-shoe-thumbnail.png' />
       </button>
-      <button className='hero-nav-thumbnail' selected={displayShoe='peach'} onClick={()=>selectShoe('peach')}>
-        <img className='hero-nav-thumbnail-image higher' alt='peach shoe thumbnail' src='/assets/backgrounds/thumbnails/peach-shoe-thumbnail.png' />
+      <button className='hero-nav-thumbnail' onClick={()=>selectShoe('peach')}>
+        <img className={'hero-nav-thumbnail-image peach ' + (displayShoe === 'peach' ? 'selected' : '')} alt='peach shoe thumbnail' src='/assets/backgrounds/thumbnails/peach-shoe-thumbnail.png' />
       </button>
-      <button className='hero-nav-thumbnail' selected={displayShoe='blue'} onClick={()=>selectShoe('blue')}>
-        <img className='hero-nav-thumbnail-image higher' alt='blue shoe thumbnail' src='/assets/backgrounds/thumbnails/blue-shoe-thumbnail.png' />
+      <button className='hero-nav-thumbnail' onClick={()=>selectShoe('blue')}>
+        <img className={'hero-nav-thumbnail-image blue ' + (displayShoe === 'blue' ? 'selected' : '')} alt='blue shoe thumbnail' src='/assets/backgrounds/thumbnails/blue-shoe-thumbnail.png' />
       </button>
     </nav>
   )

0ca228c4605f897da78d6c9f022836b8ff12b2db
Eileen Li <eileenlimur@gmail.com>
2020-11-02 02:04:25 -0500

    add conditional hamburger styling (not working yet)

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,19 @@ export default function() {
   return (
     <header>
     <a href='#' className='logo' onClick={handleSubmit}><img alt='sneaker-head logo' src='assets/logo/sn-logo-h.png'/></a>
-    <nav>
-      <button onClick={handleSubmit}>Top 10</button>
-      <button onClick={handleSubmit}>Blog</button>
-      <button onClick={handleSubmit}>About</button>
-      <button onClick={handleSubmit}>Connect</button>
-    </nav>
+    {window.innerWidth > 768 ?
+      (<nav>
+        <button onClick={handleSubmit}>Top 10</button>
+        <button onClick={handleSubmit}>Blog</button>
+        <button onClick={handleSubmit}>About</button>
+        <button onClick={handleSubmit}>Connect</button>
+      </nav>) :
+      (<div className='hamburger'>
+        <div className='hamburger-stripe'></div>
+        <div className='hamburger-stripe'></div>
+        <div className='hamburger-stripe'></div>
+      </div>)
+    }
   </header>
   )
 }

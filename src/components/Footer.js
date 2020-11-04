01990a73015a721a454d12ce9cce0576e12c75dd
Eileen Li <eileenlimur@gmail.com>
2020-11-04 00:48:08 -0500

    create separate HandleRedirect function for social buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,27 +4,31 @@ import './Footer.css';
 export default function() {
   const [email, setEmail] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleClick = (e) => {
     e.preventDefault();
     setEmail('');
+  }
+
+  const handleRedirect = (e) => {
+    e.preventDefault(); 
   }
 
   return (
     <footer>
       <div className='social-buttons'>
-        <button className='social-button' onClick={handleSubmit}>
+        <button className='social-button' onClick={handleRedirect}>
           <i className="fab fa-instagram"></i>
         </button >
-        <button className='social-button' onClick={handleSubmit}>
+        <button className='social-button' onClick={handleRedirect}>
           <i className="fab fa-twitter"></i>
         </button>
-        <button className='social-button' onClick={handleSubmit}>
+        <button className='social-button' onClick={handleRedirect}>
           <i className="fab fa-facebook-f"></i>
         </button>
       </div>
       <form>
         <input value={email} onChange={e=>setEmail(e.target.value)} placeholder='Enter Email. Stay up-to-date.'/>
-        <button onClick={handleSubmit} type='submit'>
+        <button onClick={handleClick} type='submit'>
           Signup
         </button>
       </form>
